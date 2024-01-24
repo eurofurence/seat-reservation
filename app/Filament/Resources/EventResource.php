69b25f34c2279b5eb32398b10fdc8a61b8fb38a0
Thiritin <me@thiritin.com>
2024-01-24 16:14:56 +0100

    add events

---
+++ app/Filament/Resources/EventResource.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\EventResource\Pages;
+use App\Models\Event;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class EventResource extends Resource
+{
+    protected static ?string $model = Event::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('room_id')
+                    ->relationship('room', 'name')
+                    ->required(),
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->label('Event Name')
+                    ->maxLength(255),
+                Forms\Components\DateTimePicker::make('reservation_ends_at'),
+                Forms\Components\DateTimePicker::make('starts_at')
+                    ->label('Event Start Time')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('room.name')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('reservation_ends_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('starts_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListEvents::route('/'),
+            'create' => Pages\CreateEvent::route('/create'),
+            'edit' => Pages\EditEvent::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ resources/js/Pages/Event/IndexEvent.vue
@@ -0,0 +1,55 @@
+<script setup>
+import ListItem from "@/Components/List/ListItem.vue";
+
+defineProps({
+    events: Array
+})
+import BackButton from "@/Components/BackButton.vue";
+import List from "@/Components/List/List.vue";
+import dayjs from "dayjs";
+import {computed} from "vue";
+import ChevronRight from "@/Components/Icons/ChevronRight.vue";
+import {Head} from "@inertiajs/vue3";
+
+const seatColor = (seatCount) => {
+    return seatCount < 10 ? 'text-red-500' : 'text-green-500';
+}
+</script>
+
+<template>
+    <Head title="Events" />
+    <div>
+        <BackButton :href="route('dashboard')">Back to Dashboard</BackButton>
+        <div class="mb-4 py-4 px-6">
+            <h1 class="text-2xl font-semibold">Select Event</h1>
+            <p class="text-gray-700 text-sm">Please select the event you would like to book a seat for.</p>
+        </div>
+    </div>
+    <List v-if="events.length">
+        <ListItem v-for="event in events" :href="route('bookings.create',{event: event.id})">
+            <div class="flex justify-between items-center">
+                <div class="flex justify-between items-start w-full">
+                    <div>
+                        <div>{{ event.name }}</div>
+                        <div :class="seatColor(event.seats_left)">{{ event.seats_left }} Seat left</div>
+                    </div>
+                    <div>
+                        <div>{{ event.room.name }}</div>
+                        <div>{{ dayjs(event.starts_at).format("DD.MM HH:MM") }}</div>
+                    </div>
+                </div>
+                <div class="flex items-center justify-end pl-8">
+                    <ChevronRight class="w-2"></ChevronRight>
+                </div>
+            </div>
+            <div class="text-xs">Reservation Deadline {{ dayjs(event.reservation_ends_at).format('DD.MM.YYYY - HH:mm') }}</div>
+        </ListItem>
+    </List>
+    <div v-else class="py-4 px-6">
+        <div class="text-gray-700 text-sm">There are currently no events available for booking.</div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
